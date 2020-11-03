0fa48f0ced12ea7e9e4d9923a7abca2460bf7bdd
Pawel Targosz <ptargosz@squiz.pl>
2020-11-03 18:33:39 +1100

    Deploy

--- src/pages/PublicHome/index.jsx
+++ src/pages/PublicHome/index.jsx
@@ -1,15 +1,62 @@
 import React, { useCallback, useContext, useEffect } from "react";
 import { observer } from "mobx-react-lite";
 
+import { Container, Grid } from "@material-ui/core";
+
 import { RootStoresContext } from "../../stores/RootStore";
 
 import styles from "./styles.module.scss";
 
 import InitialPage from "../InitialPage";
-import { Container, Grid } from "@material-ui/core";
+
+import { ReactComponent as IndorRowing } from "../../files/indor_rowing.svg";
+
+const ResultItem = ({ name, people, target }) => {
+  const meters = people * 500;
+  const percent = ((meters / target) * 100).toFixed(2);
+  return (
+    <Grid
+      container
+      spacing={4}
+      justify="space-between"
+      alignItems="center"
+      className={`${styles.result} ${percent >= 100 ? styles.resultDone : ""}`}
+    >
+      <Grid item className={styles.city}>
+        {name}
+      </Grid>
+      <Grid item className={styles.progressbar}>
+        <div className={styles.progress}>
+          <span className={styles.progressData}>
+            {percent < 100 ? `${percent}%` : "rekord pobity"} / {meters} m
+          </span>
+          <div
+            className={styles.bar}
+            style={{
+              width: `calc(${percent}% - 12px)`,
+            }}
+          ></div>
+        </div>
+      </Grid>
+      <Grid item className={styles.peoples}>
+        <span>{people}</span>
+        <IndorRowing className={styles.rowingIcon} />
+      </Grid>
+    </Grid>
+  );
+};
 
 const HomeView = () => {
   const rootStore = useContext(RootStoresContext);
+  const displayResults = rootStore.homeStore.page.cities.map((city) => (
+    <ResultItem
+      key={city.name}
+      name={city.name}
+      people={city.people}
+      target={rootStore.homeStore.page.target}
+    />
+  ));
+
   return (
     <Container
       role="main"
@@ -17,27 +64,16 @@ const HomeView = () => {
       maxWidth={false}
       className={styles.container}
     >
-      <Grid container spacing={3}>
-        <Grid item xs={12} className={styles.titleHeader}>
-          <svg viewBox="0 0 260 20" className={styles.svgTitle}>
-            <text x="0" y="15">
+      <Grid container spacing={4} justify="center">
+        <Grid item xs={10} className={styles.titleHeader}>
+          <svg viewBox="0 0 300 26" className={styles.svgTitle}>
+            <text x="0" y="22">
               {rootStore.homeStore.page.name}
             </text>
           </svg>
         </Grid>
-        <Grid item xs={12} className={styles.content}>
-          <Grid item xs={12}>
-            Poznan
-          </Grid>
-          <Grid item xs={12}>
-            Poznan
-          </Grid>
-          <Grid item xs={12}>
-            Poznan
-          </Grid>
-          <Grid item xs={12}>
-            Poznan
-          </Grid>
+        <Grid item xs={10} className={styles.results}>
+          {displayResults}
         </Grid>
       </Grid>
     </Container>
