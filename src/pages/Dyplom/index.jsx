61d5d2269ee73e8ede7eb3591721161f3e30728f
Pawel Targosz <ptargosz@squiz.pl>
2021-01-04 12:09:18 +1100

    Deploy Dyplom

--- src/pages/Dyplom/index.jsx
+++ src/pages/Dyplom/index.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { PDFDocument } from "pdf-lib";
 import fontkit from "@pdf-lib/fontkit";
 
-import { Box, Button, Container } from "@material-ui/core";
+import { Box, Button, Container, CircularProgress } from "@material-ui/core";
 
 import { ReactComponent as WospLogo } from "../../files/wosp_logo.svg";
 import Copyright from "../../components/Copyright";
@@ -12,9 +12,11 @@ import Copyright from "../../components/Copyright";
 import styles from "./styles.module.scss";
 
 const Dyplom = () => {
+  const [loading, setLoading] = useState(false);
   const { name } = useParams();
 
   async function modifyPdf() {
+    setLoading(true);
     const openSansBytes = await fetch("/OpenSans-Regular.ttf").then((res) =>
       res.arrayBuffer()
     );
@@ -47,16 +49,32 @@ const Dyplom = () => {
     const fileURL = URL.createObjectURL(file);
 
     window.open(fileURL);
+    setLoading(false);
   }
 
   return (
     <Container maxWidth="xs">
       <div className={styles.dyplom}>
         <WospLogo className={styles.logo} />
-        <Box mt={8}>
-          <Button variant="contained" color="primary" onClick={modifyPdf}>
+
+        {loading && (
+          <Box mt={8}>
+            <h2>Generowanie Dyplomu!</h2>
+          </Box>
+        )}
+
+        <Box mt={8} className={styles.button}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={modifyPdf}
+            disabled={loading}
+          >
             Pobierz Dyplom
           </Button>
+          {loading && (
+            <CircularProgress size={24} className={styles.progress} />
+          )}
         </Box>
       </div>
       <Box mt={8}>
